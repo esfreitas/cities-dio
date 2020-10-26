78b7206c5d532a60183d73db4eba2ceefbae6a85
esfreitas <eltons.freitas01@gmail.com>
2020-10-25 22:08:28 -0300

    atualizando API, listagem de pais ok

--- src/main/java/com/github/esfreitas/citiesapi/CountryResource.java
+++ src/main/java/com/github/esfreitas/citiesapi/CountryResource.java
@@ -1,12 +1,15 @@
 package com.github.esfreitas.citiesapi;
 import com.github.esfreitas.citiesapi.countries.Country;
 import com.github.esfreitas.citiesapi.countries.CountryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -15,12 +18,24 @@ public class CountryResource {
     private CountryRepository repository;
 
     public CountryResource(CountryRepository repository) {
+
         this.repository = repository;
     }
 
     @GetMapping
-    public List<Country> countries(){
+    public Page<Country> countries(Pageable page){
 
-        return repository.findAll();
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
  }
